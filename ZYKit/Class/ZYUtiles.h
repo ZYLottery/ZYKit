bc75dbe6f11ddae5d9f6435d19620855cf90712f
何伟东 <654500619@qq.com>
2017-02-17 10:28:36 +0800

    新增工具类

---
+++ ZYKit/Class/ZYUtiles.h
@@ -0,0 +1,20 @@
+//
+//  ZYUtiles.h
+//  ZYKit
+//
+//  Created by 何伟东 on 2017/2/17.
+//  Copyright © 2017年 章鱼彩票. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@interface ZYUtiles : NSObject
+
+/**
+ 获取app当前视图所在的viewController
+ 
+ @return <#return value description#>
+ */
++(UIViewController *)getCurrentViewController;
+
+@end
