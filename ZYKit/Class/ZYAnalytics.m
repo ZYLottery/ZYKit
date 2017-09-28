02105dcc344a9c9426f1cfa6a7cb7f6eedf97c32
何伟东 <654500619@qq.com>
2017-09-28 14:33:45 +0800

    修改神策参数

--- ZYKit/Class/ZYAnalytics.m
+++ ZYKit/Class/ZYAnalytics.m
@@ -43,6 +43,7 @@ static id sharedInstance = NULL;
     [SensorsAnalyticsSDK sharedInstanceWithServerURL:serverUrl
                                      andConfigureURL:configureUrl
                                         andDebugMode:debugMode];
+    [[SensorsAnalyticsSDK sharedInstance] identify:clientInfo.app_client_id];
     [[ZYAnalytics sharedInstance] setClientInfo:[clientInfo mj_keyValues]];
     
     // 追踪 "App 启动" 事件
